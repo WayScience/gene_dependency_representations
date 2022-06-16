fad06125a8d052b226c78b1cbee7f7ce2377b78c
mjw-18 <mjwoodnorth@gmail.com>
2022-06-16 11:41:07 -0600

    updating the data_downloader.ipynb code as well as updating the .py copy of the data_downloader code in the scripts/nbdconverted directory

--- 0.data-download/scripts/nbconverted/data_downloader.py
+++ 0.data-download/scripts/nbconverted/data_downloader.py
@@ -1,12 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# ## Download Dependency Data
+# 
+# Source: [Cancer Dependency Map resource](https://depmap.org/portal/download/).
+# 
+# - `CRISPR_gene_dependency.csv`: DESCRIBE HERE
+# - `sample_info.csv`: DESCRIBE HERE
+
+# In[2]:
 
 
+import pathlib
 import urllib.request
-CRISPR_gene_dependency = "https://ndownloader.figshare.com/files/34990033"
-sample_info = "https://ndownloader.figshare.com/files/35020903"
-urllib.request.urlretrieve(CRISPR_gene_dependency, '/home/markw/gene_dependency_representations/0.data-download/data/CRISPR_gene_dependency.csv')
-urllib.request.urlretrieve(sample_info, '/home/markw/gene_dependency_representations/0.data-download/data/sample_info.csv')
+
+
+# In[3]:
+
+
+def download_dependency_data(figshare_id, figshare_url, output_file):
+    """
+    Download the provided figshare resource
+    """
+    urllib.request.urlretrieve(f"{figshare_url}/{figshare_id}", output_file)
+
+
+# In[4]:
+
+
+# Set download constants
+output_dir = pathlib.Path("data")
+figshare_url = "https://ndownloader.figshare.com/files/"
+
+download_dict = {
+    "34990033": "CRISPR_gene_dependency.csv",
+    "35020903": "sample_info.csv"
+}
+
+
+# In[5]:
+
+
+# Make sure directory exists
+output_dir.mkdir(exist_ok=True)
+
+
+# In[6]:
+
+
+for figshare_id in download_dict:
+    # Set output file
+    output_file = pathlib.Path(output_dir, download_dict[figshare_id])
+
+    # Download the dependency data
+    print(f"Downloading {output_file}...")
+    
+    download_dependency_data(
+        figshare_id=figshare_id,
+        figshare_url=figshare_url,
+        output_file=output_file
+    )
 
