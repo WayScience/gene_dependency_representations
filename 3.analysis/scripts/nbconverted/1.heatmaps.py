1217d16388d4dde5198b421afc1dc331d88473d8
Rose <rosemdoss@gmail.com>
2023-02-24 15:32:53 -0700

    new heatmap and gsea files

---
+++ 3.analysis/scripts/nbconverted/1.heatmaps.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+import pathlib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random as python_random
+import tensorflow as tf
+import seaborn as sns
+
+sns.set_theme(color_codes=True)
+import random
+import joblib
+
+sys.path.insert(0, ".././0.data-download/scripts/")
+from data_loader import load_train_test_data, load_data
+from matplotlib.pyplot import figure, gcf
+from sklearn.decomposition import PCA
+from tensorflow import keras
+
+from keras.models import Model, Sequential
+
+
+# In[2]:
+
+
+random.seed(18)
+print(random.random())
+
+
+# In[3]:
+
+
+# load the data
+data_directory = pathlib.Path("../0.data-download/data")
+train_init, test_init, gene_stats = load_train_test_data(
+    data_directory, train_or_test="all", load_gene_stats=True
+)
+
+data_dir = "../0.data-download/data/"
+model_df, dependency_df = load_data(data_dir, adult_or_pediatric="all")
+
+
+# In[4]:
+
+
+# drop the string values
+train_df = train_init.drop(columns=["DepMap_ID", "age_and_sex"])
+test_df = test_init.drop(columns=["DepMap_ID", "age_and_sex"])
+
+
+# In[5]:
+
+
+# subsetting the genes
+# create dataframe containing the 1000 genes with the largest variances and their corresponding gene label and extract the gene labels
+largest_var_df = gene_stats.nlargest(1000, "variance")
+gene_list = largest_var_df["gene_ID"].tolist()
+gene_list
+
+# create new training and testing dataframes that contain only the corresponding genes
+subset_train_df = train_df.filter(gene_list, axis=1)
+subset_test_df = test_df.filter(gene_list, axis=1)
+
+
+# In[6]:
+
+
+train_init["train_or_test"] = train_init.apply(lambda _: "train", axis=1)
+test_init["train_or_test"] = test_init.apply(lambda _: "test", axis=1)
+
+
+# In[7]:
+
+
+# load the latent dimension dataframe
+
+latent_df = pd.read_csv("../2.train-VAE/results/latent_df.csv")
+
+print(latent_df.shape)
+
+latent_df.head(5)
+
+
+# In[8]:
+
+
+# create a data frame of both test and train gene dependency data sorted by top 1000 highest gene variances
+concat_frames = [train_init, test_init]
+train_and_test = pd.concat(concat_frames).reset_index(drop=True)
+train_and_test[["age_category", "sex"]] = train_and_test.age_and_sex.str.split(
+    pat="_", expand=True
+)
+train_and_test_subbed = train_and_test.filter(gene_list, axis=1)
+metadata_holder = []
+metadata_holder = pd.DataFrame(metadata_holder)
+metadata = metadata_holder.assign(
+    DepMap_ID=train_and_test.DepMap_ID.astype(str),
+    age_category=train_and_test.age_category.astype(str),
+    sex=train_and_test.sex.astype(str),
+    train_or_test=train_and_test.train_or_test.astype(str),
+)
+metadata
+
+
+# In[9]:
+
+
+age_category = metadata.pop("age_category")
+sex = metadata.pop("sex")
+train_test = metadata.pop("train_or_test")
+
+
+# In[10]:
+
+
+# display clustered heatmap of coefficients
+lut_pal = sns.cubehelix_palette(
+    age_category.unique().size, light=0.9, dark=0.1, reverse=True, start=1, rot=-2
+)
+put_pal = sns.cubehelix_palette(sex.unique().size)
+mut_pal = sns.color_palette("hls", train_test.unique().size)
+
+lut = dict(zip(age_category.unique(), lut_pal))
+put = dict(zip(sex.unique(), put_pal))
+mut = dict(zip(train_test.unique(), mut_pal))
+
+row_colors1 = age_category.map(lut)
+row_colors2 = sex.map(put)
+row_colors3 = train_test.map(mut)
+
+network_node_colors = pd.DataFrame(row_colors1).join(
+    pd.DataFrame(row_colors2).join(pd.DataFrame(row_colors3))
+)
+
+sns.set(font_scale=4.0)
+g = sns.clustermap(
+    latent_df.iloc[:,1:],
+    method="ward",
+    figsize=(10, 20),
+    row_colors=network_node_colors,
+    yticklabels=False,
+    dendrogram_ratio=(0.1, 0.04),
+    cbar_pos=(1, 0.3, 0.02, 0.6),
+    cmap = "coolwarm",
+)
+g.ax_row_dendrogram.set_visible(True)
+g.ax_col_dendrogram.set_visible(True)
+
+
+xx = []
+for label in age_category.unique():
+    x = g.ax_row_dendrogram.bar(0, 0, color=lut[label], label=label, linewidth=0)
+    xx.append(x)
+# add the legend
+legend3 = plt.legend(
+    xx,
+    age_category.unique(),
+    loc="center",
+    title="age category",
+    ncol=2,
+    bbox_to_anchor=(1.8, 0.91),
+    bbox_transform=gcf().transFigure,
+)
+
+
+yy = []
+for label in sex.unique():
+    y = g.ax_row_dendrogram.bar(0, 0, color=put[label], label=label, linewidth=0)
+    yy.append(y)
+# add the second legend
+legend4 = plt.legend(
+    yy,
+    sex.unique(),
+    loc="center",
+    title="sex",
+    ncol=3,
+    bbox_to_anchor=(1.8, 0.8),
+    bbox_transform=gcf().transFigure,
+)
+plt.gca().add_artist(legend3)
+
+
+zz = []
+for label in train_test.unique():
+    z = g.ax_row_dendrogram.bar(0, 0, color=mut[label], label=label, linewidth=0)
+    zz.append(z)
+# add the third legend
+legend5 = plt.legend(
+    zz,
+    train_test.unique(),
+    loc="center",
+    title="train or test",
+    ncol=2,
+    bbox_to_anchor=(1.8, 0.69),
+    bbox_transform=gcf().transFigure,
+)
+plt.gca().add_artist(legend4)
+
+
+# save the figure
+heat_save_path = pathlib.Path("../1.data-exploration/figures/heatmap.png")
+plt.savefig(heat_save_path, bbox_inches="tight", dpi=600)
+
+
+# In[11]:
+
+
+# load the weights dataframe
+
+gene_weights_df = pd.read_csv("../2.train-VAE/results/weight_matrix_encoder.csv")
+
+gene_weights_df
+
+
+# In[12]:
+
+
+sns.clustermap(
+    gene_weights_df,
+    method="ward",
+    figsize=(10, 20),
+    #network_node_colors = pd.DataFrame(row_colors1).join(pd.DataFrame(row_colors2).join(pd.DataFrame(row_colors3))
+    #row_colors=network_node_colors,
+    yticklabels=False,
+    dendrogram_ratio=(0.1, 0.04),
+    cbar_pos=(1, 0.3, 0.02, 0.6),
+    cmap = "coolwarm",
+)
+
