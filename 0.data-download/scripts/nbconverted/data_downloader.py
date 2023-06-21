32a3c313374e3f5927da96db2c4c503cbd05725d
Gregory Way <gregory.way@gmail.com>
2023-06-21 09:49:17 -0600

    add gene effect and gene metadata info to download

--- 0.data-download/scripts/nbconverted/data_downloader.py
+++ 0.data-download/scripts/nbconverted/data_downloader.py
@@ -8,6 +8,9 @@
 # - `CRISPRGeneDependency.csv`: The data in this document describes the probability that a gene knockdown has an effect on cell-inhibition or death. These probabilities are derived from the data contained in CRISPRGeneEffect.csv using methods described [here](https://doi.org/10.1101/720243)
 # - `Model.csv`: Metadata for all of DepMap’s cancer models/cell lines.
 # - `CRISPRGeneEffect.csv`: The data in this document are the Gene Effect Scores obtained from CRISPR knockout screens conducted by the Broad Institute. Negative scores notate that cell growth inhibition and/or death occurred following a gene knockout. Information on how these Gene Effect Scores were determined can be found [here](https://genomebiology.biomedcentral.com/articles/10.1186/s13059-021-02540-7)
+# - `depmap_gene_meta.tsv`: Genes that passed QC and were included in the training model for Pan et al. 2022. We use this data to filter genes as input to our models. The genes were filtered based 1) variance, 2) perturbation confidence, and 3) high on target predictions based on high correlation across other guides.
+# 
+# > Pan J, Kwon JJ, Talamas JA, Borah AA, Vazquez F, Boehm JS, Tsherniak A, Zitnik M, McFarland JM, Hahn WC. Sparse dictionary learning recovers pleiotropy from human cell fitness screens. Cell Syst. 2022 Apr 20;13(4):286-303.e10. doi: 10.1016/j.cels.2021.12.005. Epub 2022 Jan 31. PMID: 35085500; PMCID: PMC9035054.
 
 # In[1]:
 
@@ -33,7 +36,12 @@ def download_dependency_data(figshare_id, figshare_url, output_file):
 output_dir = pathlib.Path("data")
 figshare_url = "https://ndownloader.figshare.com/files/"
 
-download_dict = {"40448549": "CRISPRGeneDependency.csv", "40448834": "Model.csv"}
+download_dict = {
+    "40448549": "CRISPRGeneDependency.csv",
+    "40448555": "CRISPRGeneEffect.csv",
+    "40448834": "Model.csv",
+    "29094531": "depmap_gene_meta.tsv",  # Data from Pan et al. 2022
+}
 
 
 # In[4]:
