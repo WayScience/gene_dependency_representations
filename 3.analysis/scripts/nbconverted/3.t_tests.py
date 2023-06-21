ea77046913f8948faf8ac3c2e573ba9acbe190d9
Ecohen1763 <escohen53@gmail.com>
2023-06-21 14:31:34 -0600

    more t tests

--- 3.analysis/scripts/nbconverted/3.t_tests.py
+++ 3.analysis/scripts/nbconverted/3.t_tests.py
@@ -30,6 +30,10 @@ ped_ids = []
 adult_ids = []
 male_ids = []
 female_ids = []
+ped_male_ids = []
+adult_male_ids = []
+ped_female_ids = []
+adult_female_ids = []
 
 for index, row in metadata_df.iterrows():
     if row['AgeCategory'] == 'Pediatric':
@@ -43,6 +47,18 @@ for index, row in metadata_df.iterrows():
 
     if row['Sex'] == 'Female':
         female_ids.append(row['ModelID'])
+    
+    if row['AgeCategory'] == 'Pediatric' and row['Sex'] == 'Male':
+        ped_male_ids.append(row['ModelID'])
+
+    if row['AgeCategory'] == 'Adult' and row['Sex'] == 'Male':
+        adult_male_ids.append(row['ModelID'])
+    
+    if row['AgeCategory'] == 'Pediatric' and row['Sex'] == 'Female':
+        ped_female_ids.append(row['ModelID'])
+
+    if row['AgeCategory'] == 'Adult' and row['Sex'] == 'Female':
+        adult_female_ids.append(row['ModelID'])
 
 
 # In[4]:
@@ -57,14 +73,12 @@ for index, row in adult_latent_df.iterrows():
 adult_latent_df_float = adult_latent_df.drop(columns=["ModelID"])
 adult_latent_df_float.reset_index(drop=True, inplace=True)
 
-
 ped_latent_df = latent_df.copy()
 for index, row in ped_latent_df.iterrows():
     if row['ModelID'] not in ped_ids:
         ped_latent_df.drop(index, inplace=True)
 ped_latent_df_float = ped_latent_df.drop(columns=["ModelID"])
 ped_latent_df_float.reset_index(drop=True, inplace=True)
-
 
 male_latent_df = latent_df.copy()
 for index, row in male_latent_df.iterrows():
@@ -73,13 +87,40 @@ for index, row in male_latent_df.iterrows():
 male_latent_df_float = male_latent_df.drop(columns=["ModelID"])
 male_latent_df_float.reset_index(drop=True, inplace=True)
 
-
 female_latent_df = latent_df.copy()
 for index, row in female_latent_df.iterrows():
     if row['ModelID'] not in female_ids:
         female_latent_df.drop(index, inplace=True)
 female_latent_df_float = female_latent_df.drop(columns=["ModelID"])
 female_latent_df_float.reset_index(drop=True, inplace=True)
+
+ped_male_latent_df = latent_df.copy()
+for index, row in ped_male_latent_df.iterrows():
+    if row['ModelID'] not in ped_male_ids:
+        ped_male_latent_df.drop(index, inplace=True)
+ped_male_latent_df_float = ped_male_latent_df.drop(columns=["ModelID"])
+ped_male_latent_df_float.reset_index(drop=True, inplace=True)
+
+adult_male_latent_df = latent_df.copy()
+for index, row in adult_male_latent_df.iterrows():
+    if row['ModelID'] not in adult_male_ids:
+        adult_male_latent_df.drop(index, inplace=True)
+adult_male_latent_df_float = adult_male_latent_df.drop(columns=["ModelID"])
+adult_male_latent_df_float.reset_index(drop=True, inplace=True)
+
+ped_female_latent_df = latent_df.copy()
+for index, row in ped_female_latent_df.iterrows():
+    if row['ModelID'] not in ped_female_ids:
+        ped_female_latent_df.drop(index, inplace=True)
+ped_female_latent_df_float = ped_female_latent_df.drop(columns=["ModelID"])
+ped_female_latent_df_float.reset_index(drop=True, inplace=True)
+
+adult_female_latent_df = latent_df.copy()
+for index, row in adult_female_latent_df.iterrows():
+    if row['ModelID'] not in adult_female_ids:
+        adult_female_latent_df.drop(index, inplace=True)
+adult_female_latent_df_float = adult_female_latent_df.drop(columns=["ModelID"])
+adult_female_latent_df_float.reset_index(drop=True, inplace=True)
 
 
 # In[5]:
@@ -108,4 +149,60 @@ print(t_test_male_vs_female.shape)
 
 # Sort to show latent dimensions with most significant p values
 t_test_male_vs_female.sort_values(by='p_value', ascending = True)
+
+
+# In[7]:
+
+
+# t tests comparing adult male vs ped male for each latent dimension
+
+t_test_adult_male_vs_ped_male = ttest_ind(ped_male_latent_df_float, adult_male_latent_df_float)
+t_test_adult_male_vs_ped_male = pd.DataFrame(t_test_adult_male_vs_ped_male).T
+t_test_adult_male_vs_ped_male.columns = ["t_stat", "p_value"]
+print(t_test_adult_male_vs_ped_male.shape)
+
+# Sort to show latent dimensions with most significant p values
+t_test_adult_male_vs_ped_male.sort_values(by='p_value', ascending = True)
+
+
+# In[8]:
+
+
+# t tests comparing adult female vs ped female for each latent dimension
+
+t_test_adult_female_vs_ped_female = ttest_ind(ped_female_latent_df_float, adult_female_latent_df_float)
+t_test_adult_female_vs_ped_female = pd.DataFrame(t_test_adult_female_vs_ped_female).T
+t_test_adult_female_vs_ped_female.columns = ["t_stat", "p_value"]
+print(t_test_adult_female_vs_ped_female.shape)
+
+# Sort to show latent dimensions with most significant p values
+t_test_adult_female_vs_ped_female.sort_values(by='p_value', ascending = True)
+
+
+# In[9]:
+
+
+# t tests comparing ped male vs ped female for each latent dimension
+
+t_test_ped_male_vs_ped_female = ttest_ind(ped_female_latent_df_float, ped_male_latent_df_float)
+t_test_ped_male_vs_ped_female = pd.DataFrame(t_test_ped_male_vs_ped_female).T
+t_test_ped_male_vs_ped_female.columns = ["t_stat", "p_value"]
+print(t_test_ped_male_vs_ped_female.shape)
+
+# Sort to show latent dimensions with most significant p values
+t_test_ped_male_vs_ped_female.sort_values(by='p_value', ascending = True)
+
+
+# In[10]:
+
+
+# t tests comparing adult male vs adult female for each latent dimension
+
+t_test_adult_male_vs_adult_female = ttest_ind(adult_female_latent_df_float, adult_male_latent_df_float)
+t_test_adult_male_vs_adult_female = pd.DataFrame(t_test_adult_male_vs_adult_female).T
+t_test_adult_male_vs_adult_female.columns = ["t_stat", "p_value"]
+print(t_test_adult_male_vs_adult_female.shape)
+
+# Sort to show latent dimensions with most significant p values
+t_test_adult_male_vs_adult_female.sort_values(by='p_value', ascending = True)
 
